a60f2de908face3c9fdec99d2929b14861b48c31
maxicoltro <maxicoltro@LAPTOP-COO7V55K.localdomain>
2022-02-18 16:51:54 -0300

    0-first-commit

--- search_news.py
+++ search_news.py
@@ -1,10 +1,10 @@
-from unittest import result
-from wsgiref import headers
+# from unittest import result
+# from wsgiref import headers
 import config
 import params
 import aiohttp
 import asyncio
-import os
+# import os
 import json
 
 
@@ -20,31 +20,9 @@ def get_source():
     return params.source
 
 
-async def get_top_headlines(session, url, q=None, country='ar', category=None, api_key=None):
-    if api_key is None:
-            api_key = config.api_key_newsapi
+async def get_top_headlines(session, url, q=None, country='ar', category=None,
+                            api_key=None):
 
-    headers = {
-        'accept': 'application/json',
-        'X-Api-Key': api_key
-    }
-    
-    if country is None:
-        country = get_country()
-
-    if category is None:
-        category = get_category()
-
-    async with aiohttp.ClientSession(headers=headers) as session:
-        
-        async with session.get(url=url) as resp:
-            print(resp.status)
-            res = await resp.json()
-            # print(res)
-
-            return res
-
-async def get_all_requests(session, urls, api_key=None):
     if api_key is None:
         api_key = config.api_key_newsapi
 
@@ -53,23 +31,50 @@ async def get_all_requests(session, urls, api_key=None):
         'X-Api-Key': api_key
     }
 
+    if country is None:
+        country = get_country()
+
+    if category is None:
+        category = get_category()
+
+    async with aiohttp.ClientSession(headers=headers) as session:
+
+        async with session.get(url=url) as resp:
+            print(resp.status)
+            res = await resp.json()
+            # print(res)
+
+            return res
+
+
+async def get_all_requests(session, urls, api_key=None):
+    if api_key is None:
+        api_key = config.api_key_newsapi
+
+    """headers = {
+        'accept': 'application/json',
+        'X-Api-Key': api_key
+    }"""
+
     for url in urls:
         tasks = []
         task = asyncio.create_task(get_top_headlines(session, url))
         tasks.append(task)
 
     results = await asyncio.gather(*tasks)
-    
+
     return results
+
 
 async def main(urls, api_key=None):
     if api_key is None:
-            api_key = config.api_key_newsapi
+        api_key = config.api_key_newsapi
 
-    headers = {
+    """headers = {
         'accept': 'application/json',
         'X-Api-Key': api_key
-    }
+    }"""
+
     async with aiohttp.ClientSession() as session:
         data = await get_all_requests(session, urls)
 
@@ -82,9 +87,10 @@ if __name__ == '__main__':
 
     results = asyncio.get_event_loop().run_until_complete(main(urls))
 
-    #print(type(results))
-    #print(results[0])
-    print('{} top headlines for Argentina'.format(int(results[0]['totalResults'])))
+    # print(type(results))
+    # print(results[0])
+    print('{} top headlines for Argentina'.format(
+            int(results[0]['totalResults'])))
 
     articles_dict = [a for a in results[0]['articles']]
 
@@ -96,6 +102,6 @@ if __name__ == '__main__':
 
     # print(dict_news)
     # print(len(dict_news))
-    
+
     with open("data/top_headlines.json", "w") as jsonfile:
         json.dump(articles_dict, jsonfile, indent=1)
