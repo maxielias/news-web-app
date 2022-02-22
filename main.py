46732e312c367f1739ad07f3a109c88cc6aea913
maxicoltro <maxicoltro@LAPTOP-COO7V55K.localdomain>
2022-02-21 23:56:18 -0300

    5-use-moralis-when-creating-snapshot

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template
+# from flask_sqlalchemy import SQLAlchemy
 import json
-# import os
-# import search_news
+import os
+import search_news
+import asyncio
 
 app = Flask(__name__)
+# db = SQLAlchemy(app)
 
 
 @app.route("/")
 @app.route("/home")
-def home():
-    return render_template("home.html")
+async def home():
+    await search_news.update_top_headers()
+    asyncio.sleep(1.0)
+    
+    with open("data/top_headlines.json", "r") as jsonfile:
+        top_headlines_json = json.load(jsonfile)
+
+    return render_template("home.html",
+                        top_headlines_json=top_headlines_json,
+                        top_headlines_titles=[a['title'] for a in top_headlines_json],
+                        top_headlines_url=[a['url'] for a in top_headlines_json],
+                        n_articles=len([a['title'] for a in top_headlines_json]))
 
 
 @app.route("/top_headlines")
@@ -18,7 +31,9 @@ def get_top_headlines():
         top_headlines_json = json.load(jsonfile)
 
     return render_template("top_headlines.html",
-                           top_headlines_title=top_headlines_json)
+                           top_headlines_json=top_headlines_json,
+                           top_headlines_titles=[a['title'] for a in top_headlines_json],
+                           n_articles=len([a['title'] for a in top_headlines_json]))
 
 
 @app.route("/headlines")
@@ -27,4 +42,9 @@ def top_headlines():
 
 
 if __name__ == "__main__":
+    
     app.run(host="127.0.0.1", port=5000, debug=True)
+
+    """while True:
+        
+        search_news.update_top_headers()"""
