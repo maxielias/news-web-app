9a7e2a5331cac4c5d35ad1a96b79c221a1ef2bc6
maxielias <maximilianocoltro@gmail.com>
2022-03-25 10:25:04 -0300

    commit full page with correct style

--- params.py
+++ params.py
@@ -1,8 +1,15 @@
-country = ['ae', 'ar', 'at', 'au', 'be', 'bg', 'br', 'ca', 'ch', 'cn', 'co',
-           'cu', 'cz', 'de', 'eg', 'fr', 'gb', 'gr', 'hk', 'hu', 'id', 'ie',
-           'il', 'in', 'it', 'jp', 'kr', 'lt', 'lv', 'ma', 'mx', 'my', 'ng',
-           'nl', 'no', 'nz', 'ph', 'pl', 'pt', 'ro', 'rs', 'ru', 'sa', 'se',
-           'sg', 'si', 'sk', 'th', 'tr', 'tw', 'ua', 'us', 've', 'za']
+country = [('AR', 'Argentina'), ('AU', 'Australia'), ('AT', 'Austria'), ('BE', 'Belgium'), 
+           ('BR', 'Brazil'), ('BG', 'Bulgaria'), ('CA', 'Canada'), ('CN', 'China'), ('CO', 'Colombia'), 
+           ('CU', 'Cuba'), ('CZ', 'Czech Republic'), ('EG', 'Egypt'), ('FR', 'France'), ('DE', 'Germany'), 
+           ('GR', 'Greece'), ('HK', 'Hong Kong'), ('HU', 'Hungary'), ('IN', 'India'), ('ID', 'Indonesia'), 
+           ('IE', 'Ireland'), ('IL', 'Israel'), ('IT', 'Italy'), ('JP', 'Japan'), ('LV', 'Latvia'), 
+           ('LT', 'Lithuania'), ('MY', 'Malaysia'), ('MX', 'Mexico'), ('MA', 'Morocco'), ('NL', 'Netherlands'), 
+           ('NZ', 'New Zealand'), ('NG', 'Nigeria'), ('NO', 'Norway'), ('PH', 'Philippines'), ('PL', 'Poland'), 
+           ('PT', 'Portugal'), ('RO', 'Romania'), ('RU', 'Russia'), ('SA', 'Saudi Arabia'), ('RS', 'Serbia'), 
+           ('SG', 'Singapore'), ('SK', 'Slovakia'), ('SI', 'Slovenia'), ('ZA', 'South Africa'), 
+           ('KR', 'South Korea'), ('SE', 'Sweden'), ('CH', 'Switzerland'), ('TW', 'Taiwan'), ('TH', 'Thailand'), 
+           ('TR', 'Turkey'), ('UA', 'Ukraine'), ('AE', 'United Arab Emirates'), ('GB', 'United Kingdom'), 
+           ('US', 'United States'), ('VE', 'Venezuela')]
 category = ['business', 'entertainment', 'general', 'health', 'science',
             'sports', 'technology']
 source = ['nan']
--- test.py
+++ test.py
@@ -1,30 +1,58 @@
 # import os
 import json
 import re
+import params
+import search_news
+import asyncio
+import aiohttp
+from concurrent.futures import CancelledError
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
-with open("data/top_headlines.json", "r") as jsonfile:
-    top_headlines_json = json.load(jsonfile)
+a = [c for c in params.country]
+# urls = ['https://newsapi.org/v2/top-headlines?country={}'.format(c[0]) for c in a]
+urls = ['https://newsapi.org/v2/top-headlines?country=us']
 
-title = [a['title'] for a in top_headlines_json]
-"""list_title_agg = []
-for t in range(0, len(title), 4):
-        start = t
-        end = t + 3
-        list_title_agg.append(title[start:end])
+search_news.replace_null_img()
 
-print(list_title_agg)
-print(len(list_title_agg))
-print(len(list_title_agg[1]))"""
+"""async def main():
+    try:
 
-#print(title[0])
-#print(top_headlines_json[0]['source']['name'])
-#print(title[0].lower().replace(' - ' + top_headlines_json[0]['source']['name'].lower(), ''))
+        #token_wallet_balances.get_latest_blocks()
+        await search_news.update_top_headlines()
+        # token_wallet_balances.moralis_url(chain_list, token_list, wallet_list)
 
-# print(re.split('-', title[0])[-1].lower().replace(top_headlines_json[0]['source']['name'].lower(), ''))
-# print(len(re.split('-', title[0])))
-# print(re.findall(r'[^\w]{2,}', title[0])[-1])
-# print(re.split(re.findall(r'[^\w]{2,}', title[0])[-1], title[0])[:-1])
-# print(" ".join(re.split('-', title[0])[:-1]).lstrip(' ').rstrip(' '))
-source = list(set([s['source']['name'] for s in top_headlines_json]))
-title = [a['title'].split(' - ')[0] for a in top_headlines_json]
-print(len(title))
+    except CancelledError:
+        raise CancelledError
+
+    except Exception as e:
+        logging.exception(e)
+        raise e
+        # return None
+
+
+if __name__ == "__main__":
+
+    # asyncio.run(main())
+    asyncio.get_event_loop().run_until_complete(main())
+    # balances = asyncio.get_event_loop().run_until_complete(main())
+    # print(balances)"""
+
+"""with open('data/top_headlines_us.json', 'r', encoding="utf8") as jsonfile:
+    jsonfile = json.load(jsonfile)
+
+    replace_null = 'https://imgs.search.brave.com/Ia4xwMxHraroG2AhGFyeaM5KLPQjGCu1924x_5U5Rq8/rs:fit:640:400:1/g:ce/aHR0cHM6Ly9hcnRz/bWlkbm9ydGhjb2Fz/dC5jb20vd3AtY29u/dGVudC91cGxvYWRz/LzIwMTQvMDUvbm8t/aW1hZ2UtYXZhaWxh/YmxlLWljb24tNi5w/bmc'          
+
+    for uti in jsonfile['articles']:
+        if uti['urlToImage'] == None:
+            uti['urlToImage'] = replace_null
+            print("None value found")
+    # url_to_image = [a['urlToImage'] == replace_null for a in jsonfile['articles'] if a['urlToImage'] == None]
+
+    print(jsonfile)"""
+
+"""print([a for a in jsonfile['articles']])
+print([a['title'].split(' - ')[0] for a in jsonfile['articles']])"""
+
+
+'https://search.brave.com/images?q=Republicans%20turn%20Ketanji%20Brown%20Jackson%20hearing%20into%20a%20political%20circus%20-%20The%20Guardian'
